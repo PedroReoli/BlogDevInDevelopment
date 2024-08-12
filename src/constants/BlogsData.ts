2ec144ee36128357070c3ebf3c33f0153876bb8f
PedroReoli <pedrolucas2012@gmail.com>
2024-08-12 18:52:30 -0300

    Update BlogsData.ts

--- src/constants/BlogsData.ts
+++ src/constants/BlogsData.ts
@@ -4,7 +4,7 @@ export const blogsData: BlogPost[] = [
   {
     title: "Primeiro Post",
     description: "Descrição do primeiro post.",
-    date: "2024-01-01",
+    date: "12/08/2024",
     keywords: ["React", "JavaScript"],
     imageUrl: "/imagens/primeiro-post.jpg",
     filename: "post1" // Deve corresponder a "post1.html" em /content/
@@ -12,7 +12,7 @@ export const blogsData: BlogPost[] = [
   {
     title: "Segundo Post",
     description: "Descrição do segundo post.",
-    date: "2024-01-02",
+    date: "12/08/2024",
     keywords: ["CSS", "Design"],
     imageUrl: "/imagens/segundo-post.jpg",
     filename: "post2" // Deve corresponder a "post2.html" em /content/
@@ -20,7 +20,7 @@ export const blogsData: BlogPost[] = [
   {
     title: "Terceuri Post",
     description: "Descrição do segundo post.",
-    date: "2024-01-02",
+    date: "12/08/2024",
     keywords: ["CSS", "Design"],
     imageUrl: "/imagens/segundo-post.jpg",
     filename: "post2" // Deve corresponder a "post2.html" em /content/
