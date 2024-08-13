4b0c9e64ffd632c382a33065cb9e0129ee0ae65a
PedroReoli <pedrolucas2012@gmail.com>
2024-08-12 22:57:19 -0300

    Update Topbar.tsx

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,20 +1,23 @@
 // src/components/Topbar.tsx
+
 import { Link } from 'react-router-dom';
 
 const Topbar = () => {
   return (
-    <header className="bg-gradient-to-b from-black to-[#111111] p-4 sticky top-0 z-50 shadow-md border-b-2 border-gray-700">
-        <img src="/assets/logo.svg" alt="logo" className="w-36 h-auto" />
-      <div className="container mx-auto flex justify-between items-center">
-        <nav className="hidden md:flex space-x-6">
-          <Link to="/" className="text-white hover:text-blue-500 transition">Home</Link>
-          <a href="#" className="text-white hover:text-blue-500 transition">Portfolio</a>
-          <a href="#experience" className="text-white hover:text-blue-500 transition">Material</a>
-          <a href="#projects" className="text-white hover:text-blue-500 transition">Twitter</a>
-          <a href="#services" className="text-white hover:text-blue-500 transition">Cursos</a>
-        </nav>
+    <div className="bg-gradient-to-b from-black to-[#111111]  text-white p-4 flex justify-between items-center fixed top-0 left-0 right-0 z-50 border-b-2 border-gray-700">
+      {/* Nome Pedro Reoli */}
+      <div className="text-2xl font-bold">
+        &lt; PedroReoli /&gt;
       </div>
-    </header>
+
+      {/* Menu */}
+      <div className="flex space-x-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/portfolio" className="hover:underline">Portfolio</Link>
+        <Link to="https://x.com/opedroreoli" className="hover:underline">Twitter</Link>
+        {/* <Link to="/cursos" className="hover:underline">Cursos</Link> */}
+      </div>
+    </div>
   );
 };
 
