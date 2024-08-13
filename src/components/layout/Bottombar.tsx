d3e350016e09b9eb09c5a990d04569760892d379
PedroReoli <pedrolucas2012@gmail.com>
2024-08-12 23:15:14 -0300

    a

--- src/components/layout/Bottombar.tsx
+++ src/components/layout/Bottombar.tsx
@@ -1,14 +1,33 @@
-// src/components/Bottombar.tsx
+import { useState } from "react";
+import Popup from "@/components/pop-ups/Popup";
 
 const Bottombar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState("");
+
+  const handleOpen = (contentType: string) => {
+    setContent(contentType);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setContent("");
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <footer className="bg-gradient-to-b from-black to-[#111111] p-4 md:hidden fixed bottom-0 w-full z-50 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-      <a href="#home" className="text-white hover:text-blue-500 transition">Sobre</a>
-          <a href="#about" className="text-white hover:text-blue-500 transition">Blog</a>
-          <a href="#experience" className="text-white hover:text-blue-500 transition">Material</a>
-          <a href="#projects" className="text-white hover:text-blue-500 transition">Twitter</a>
-          <a href="#services" className="text-white hover:text-blue-500 transition">Cursos</a>  </div>
+        <a href="#" onClick={() => handleOpen("Contribua")} className="text-white hover:text-blue-500 transition">Contribua</a>
+        <a href="#" onClick={scrollToTop} className="text-white hover:text-blue-500 transition">Inicio</a>
+        <a href="#" onClick={() => handleOpen("Contato")} className="text-white hover:text-blue-500 transition">Contato</a>
+      </div>
+
+      <Popup isOpen={isOpen} content={content} onClose={handleClose} />
     </footer>
   );
 };
--- src/components/pop-ups/Popup.tsx
+++ src/components/pop-ups/Popup.tsx
@@ -8,59 +8,87 @@ interface PopupProps {
 
 const Popup = ({ isOpen, content, onClose }: PopupProps) => {
   const getContent = () => {
-    if (content === "Termos de Serviço") {
-      return (
-        <>
-          <p>
-            Estes termos de serviço governam o uso deste site. Ao acessá-lo, você concorda com os termos descritos aqui. Se você não concorda com qualquer parte destes termos, não deve utilizar este site.
-          </p>
-          <p>
-            Reservamo-nos o direito de modificar estes termos a qualquer momento, portanto, revisite esta página periodicamente.
-          </p>
-        </>
-      );
-    } else if (content === "Privacidade") {
-      return (
-        <>
-          <p>
-            Este blog não solicita, armazena ou processa quaisquer dados pessoais para o seu acesso ou uso. O propósito deste site é exclusivamente informativo, sem oferecer interações com usuários ou a necessidade de inserção de dados pessoais.
-          </p>
-          <p>
-            Por conseguinte, asseguramos que não há qualquer possibilidade de comprometimento de seus dados pessoais através deste blog.
-          </p>
-        </>
-      );
-    } else if (content === "Chave Pix copiada com sucesso") {
-      return (
-        <>
-          <p>
-           VALEU !!!!
-          </p>
-        </>
-      );
+    switch (content) {
+      case "Termos de Serviço":
+        return (
+          <>
+            <p>
+              Estes termos de serviço governam o uso deste site. Ao acessá-lo, você concorda com os termos descritos aqui. Se você não concorda com qualquer parte destes termos, não deve utilizar este site.
+            </p>
+            <p>
+              Reservamo-nos o direito de modificar estes termos a qualquer momento, portanto, revisite esta página periodicamente.
+            </p>
+          </>
+        );
+
+      case "Privacidade":
+        return (
+          <>
+            <p>
+              Este blog não solicita, armazena ou processa quaisquer dados pessoais para o seu acesso ou uso. O propósito deste site é exclusivamente informativo, sem oferecer interações com usuários ou a necessidade de inserção de dados pessoais.
+            </p>
+            <p>
+              Por conseguinte, asseguramos que não há qualquer possibilidade de comprometimento de seus dados pessoais através deste blog.
+            </p>
+          </>
+        );
+
+      case "Chave Pix copiada com sucesso":
+        return (
+          <>
+            <p>VALEU !!!!</p>
+          </>
+        );
+
+      case "Contribua":
+        return (
+          <>
+            <ul className="text-center text-lg space-y-4">
+              <li><a href="mailto:pedrosousa2160@gmail.com" className="text-blue-400 hover:underline">Reportar um bug</a></li>
+              <li><a href="https://github.com/PedroReoli" className="text-blue-400 hover:underline">Github</a></li>
+              <li><a href="#" onClick={() => navigator.clipboard.writeText('87ed50aa-9526-46a2-8aec-e1a1cce4a9e4')} className="text-blue-400 hover:underline">Copiar chave Pix</a></li>
+              <li><a href="#" className="text-blue-400 hover:underline">Patreon</a></li>
+            </ul>
+          </>
+        );
+
+      case "Contato":
+        return (
+          <>
+            <ul className="text-center text-lg space-y-4">
+              <li><a href="https://x.com/opedroreoli" className="text-blue-400 hover:underline">Twitter</a></li>
+              <li><a href="https://www.youtube.com/@DevDesenvolvimento" className="text-blue-400 hover:underline">YouTube</a></li>
+              <li><a href="https://www.instagram.com/01_dev_em_desenvolvimento" className="text-blue-400 hover:underline">Instagram</a></li>
+              <li><a href="mailto:pedrosousa2160@gmail.com" className="text-blue-400 hover:underline">Email</a></li>
+            </ul>
+          </>
+        );
+
+      default:
+        return null;
     }
-    return null;
   };
 
   return (
     <>
       {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 z-50">
           <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 50 }}
-            className="bg-gradient-to-r from-blue-600 via-blue-700 to-blue-800 text-white p-6 md:p-8 rounded-2xl shadow-lg max-w-xs sm:max-w-sm mx-auto"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            className="bg-[#1a1a1a] text-white p-10 md:p-12 rounded-2xl shadow-lg max-w-lg sm:max-w-xl mx-auto border border-blue-500 neon-border"
+            style={{ boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)" }}
           >
-            <h2 className="text-2xl md:text-3xl font-extrabold text-center mb-4">{content}</h2>
-            <div className="text-white text-center">
+            <h2 className="text-3xl md:text-4xl font-extrabold text-center text-blue-400 mb-8">{content}</h2>
+            <div className="text-gray-300 text-center">
               {getContent()}
             </div>
             {content !== "Chave Pix copiada com sucesso" && (
-              <div className="flex justify-center mt-4">
-                <button 
+              <div className="flex justify-center mt-8">
+                <button
                   onClick={onClose}
-                  className="bg-white text-blue-700 font-semibold py-2 px-4 rounded-lg hover:bg-gray-200 transition-colors shadow-md"
+                  className="bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg hover:bg-blue-600 transition-colors shadow-md"
                 >
                   Fechar
                 </button>
