050526555c1e54461114a86e815c396583af15a4
PedroReoli <pedrolucas2012@gmail.com>
2025-04-18 15:12:11 -0300

    New Architecture and Clean Code

--- src/components/_navigation/LessonNavigation.tsx
+++
@@ -1,8 +0,0 @@
-import { lessonsData } from "@/constants/LessonsData";
-import Navigation from "@/components/Shared/Navigation";
-
-const LessonNavigation: React.FC<{ currentSlug: string }> = ({ currentSlug }) => {
-  return <Navigation currentSlug={currentSlug} data={lessonsData} basePath="lesson" />;
-};
-
-export default LessonNavigation;
--- src/components/_navigation/PostNavigation.tsx
+++
@@ -1,8 +0,0 @@
-import { blogsData } from "@/constants/BlogsData";
-import Navigation from "@/components/Shared/Navigation";
-
-const PostNavigation: React.FC<{ currentSlug: string }> = ({ currentSlug }) => {
-  return <Navigation currentSlug={currentSlug} data={blogsData} basePath="post" />;
-};
-
-export default PostNavigation;
